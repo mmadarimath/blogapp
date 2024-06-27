bc82e9cec843c7067521f193e3f9c1c55acc5e99
Mrutyunjay Madarimath <mrutyunjay69@gmail.com>
2024-06-27 22:43:06 +0530

    commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-red-500'>hello</h1>
+    </div>
+  )
+}
+
+export default App;
+
